de987ecb68e87b22119ff7c0d85dfe220a624314
Kati Tatseos <katitatseos@gmail.com>
2021-01-16 15:40:26 -0600

    Animation timing

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,23 @@ jQuery(document).ready(function ($) {
             if (target.length) {
                 $('html,body').animate({
                     scrollTop: (target.offset().top - 80)
-                }, 1000);
+                }, 800);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+
+    $('#home-btns').find('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 80)
+                }, 800);
                 if ($('.navbar-toggle').css('display') != 'none') {
                     $(this).parents('.container').find(".navbar-toggle").trigger("click");
                 }
